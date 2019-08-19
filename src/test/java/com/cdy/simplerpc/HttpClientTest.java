5a45b7f4be80f53f4b8de599e4bf7dbcad08fa89
cdy1996 <chendongyi1996@gmail.com>
2019-08-19 22:41:51 +0800

    说明: httpclient demo测试

--- src/test/java/com/cdy/simplerpc/HttpClientTest.java
+++ src/test/java/com/cdy/simplerpc/HttpClientTest.java
@@ -1,8 +1,16 @@
 package com.cdy.simplerpc;
 
+import com.cdy.simplerpc.exception.RPCException;
 import com.cdy.simplerpc.remoting.ClusterClient;
 import com.cdy.simplerpc.remoting.http.HttpClientUtil;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.EntityBuilder;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
 import org.junit.Test;
+
+import java.io.IOException;
 
 /**
  * httpclient测试
@@ -17,6 +25,32 @@ public class HttpClientTest {
     public void test() throws Exception {
         byte[] execute = HttpClientUtil.execute(HttpClientUtil.getHttpClient(), "http://localhost:80/gradle/count", null, 1000);
         System.out.println(new String(execute));
+    }
+    
+    @Test
+    public void execute(){
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectTimeout(50000)  //连接时间
+                .setConnectionRequestTimeout(50000) //connect Manager(连接池)获取Connection 超时时间
+                .setSocketTimeout(50000) //请求获取数据的超时时间(即响应时间)
+                .build();
+        HttpPost httpPost = new HttpPost("http://127.0.0.1:8080/simpleRPC");
+        httpPost.setConfig(requestConfig);
+        httpPost.setEntity(EntityBuilder.create().setBinary("weeqeqw".getBytes()).setContentEncoding("UTF-8").build());
+//        httpPost.setHeader("Content-Type", "multipart/form-data");
+    
+//        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+//        builder.addTextBody("json", jsonParam.toString(), ContentType.MULTIPART_FORM_DATA);
+    
+        CloseableHttpResponse response = null;
+        try {
+            response = HttpClientUtil.getHttpClient().execute(httpPost);
+        } catch (IOException e) {
+            throw new RPCException(e);
+        }
+    
+        HttpEntity entity = response.getEntity();
+        System.out.println(entity);
     }
     
     public static void main(String[] args) {
---
+++ src/test/java/com/cdy/simplerpc/remoting/http/HttpServerTest.java
@@ -0,0 +1,16 @@
+package com.cdy.simplerpc.remoting.http;
+
+import com.cdy.simplerpc.remoting.ServerMetaInfo;
+import com.cdy.simplerpc.serialize.JsonSerialize;
+import org.junit.Test;
+
+public class HttpServerTest {
+    
+    @Test
+    public void openServer() throws Exception {
+        HttpServer http = new HttpServer(new ServerMetaInfo("http", "8080", "127.0.0.1"), null, new JsonSerialize());
+        http.openServer();
+    
+        System.in.read();
+    }
+}
